d85378c28132a6addcc757ae9f8450a3a1021886
brorae <jhr3239@gmail.com>
2022-02-23 18:11:23 +0900

    220223

---
+++ src/크레인인형뽑기게임/Solution.java
@@ -0,0 +1,35 @@
+package 크레인인형뽑기게임;
+
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        Stack<Integer> basket = new Stack<>();
+
+        int answer = 0;
+
+        for (int i = 0; i < moves.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                int column = moves[i] - 1;
+                int item = board[j][column];
+                if (item != 0) {
+                    if (basket.empty()) {
+                        basket.push(item);
+                        board[j][column] = 0;
+                        break;
+                    }
+                    if (basket.peek() == item) {
+                        answer+=2;
+                        basket.pop();
+                    }
+                    else {
+                        basket.push(item);
+                    }
+                    board[j][column] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
