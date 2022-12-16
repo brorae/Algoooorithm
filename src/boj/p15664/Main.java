361846176cfe6d76a1d2e900fda0e895589e00b5
brorae <jhr3239@gmail.com>
2022-12-16 11:02:35 +0900

    221216

---
+++ src/boj/p15664/Main.java
@@ -0,0 +1,61 @@
+package boj.p15664;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static boolean[] visit;
+    static int[] result;
+    static Set<String> set = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        visit = new boolean[N];
+        result = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+
+        bw.write(String.join("\n", set));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+
+    static void dfs(int a, int start) {
+        if (a == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            set.add(sb.toString());
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            result[a] = arr[i];
+            dfs(a + 1, i + 1);
+        }
+    }
+}
