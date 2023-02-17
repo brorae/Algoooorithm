176c4ff63c81b008cb35c2dde31afad39b15ff7b
brorae <jhr3239@gmail.com>
2023-02-17 13:45:11 +0900

    230217

---
+++ src/boj/p2748/Main.java
@@ -0,0 +1,31 @@
+package boj.p2748;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        dp = new long[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        bw.write(dp[n] + "");
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
