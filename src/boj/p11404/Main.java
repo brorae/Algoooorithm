0262f2ff602c6e07662b3921971d3bc8587c3f64
brorae <jhr3239@gmail.com>
2023-02-26 17:52:37 +0900

    230226

---
+++ src/boj/p11404/Main.java
@@ -0,0 +1,66 @@
+package boj.p11404;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static long[][] cost;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        cost = new long[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i != j) {
+                    cost[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            cost[a][b] = Math.min(cost[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i != j) {
+                        long a = cost[i][j];
+                        long b = cost[i][k] + cost[k][j];
+                        cost[i][j] = Math.min(a, b);
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (cost[i][j] == Integer.MAX_VALUE) {
+                    bw.write("0 ");
+                } else {
+                    bw.write(cost[i][j] + " ");
+                }
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+
+        bw.close();
+        br.close();
+
+    }
+}
